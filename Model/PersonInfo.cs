bc6ae8edcd0a32333001c85ee2393aa4241e2608
tshaked <tal.shaked@gettyimages.com>
2022-04-13 13:44:07 -0500

    Favicon + add more controls on UI

--- Model/PersonInfo.cs
+++ Model/PersonInfo.cs
@@ -2,17 +2,6 @@
 {
     public class PersonInfo
     {
-        public string Id
-        {
-            get
-            {
-                return $"{Name} - {Company}";
-            }
-            set
-            {
-
-            }
-        }
         public string? Company { get; set; }
         public string? Name { get; set; }
         public int? Age { get; set; }
---
+++ Model/PersonInfoUI.cs
@@ -0,0 +1,24 @@
+﻿namespace AntDesignExample.Model
+{
+    public class PersonInfoUI : PersonInfo
+    {
+        public PersonInfoUI(PersonInfo personInfo)
+        {
+            Age = personInfo.Age;
+            Company = personInfo.Company;
+            Name = personInfo.Name;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return $"{Name} - {Company}";
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
